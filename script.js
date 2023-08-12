4a793f42ebbff8c86118917ccbea039ca667fe1e
xuexiaoying <camilla.xue@mail.utoronto.ca>
2023-08-12 18:42:26 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((element) => {
+        if (isInViewport(element)) {
+            element.classList.add('fade-in-visible');
+        }
+    });
+}
+
+// Attach the scroll event listener
+window.addEventListener('scroll', handleScroll);
+
+/*
 import React from 'react';
 import './css/spe_f.css';
 
@@ -21,3 +46,4 @@ function FadeInSection(props){
         </div>
         );
     }
+*/
