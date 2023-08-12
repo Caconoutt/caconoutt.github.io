1aa3c668bfbf60d24db75d238691e117c90241a4
xuexiaoying <camilla.xue@mail.utoronto.ca>
2023-08-11 21:44:45 -0400

    test

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-$(document).on(
-    "scroll", function(){
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
+function FadeInSection(props){
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+        const observer = new IntersectionObserver(entries =>{
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        observer.observe(domRef.current);
+        return () => observer.unobserve(domRef.current);
+    }, []);
+    return(
+        <div
+            className={`fade-in-section ${isVisible ? 'isVisible' : ''}`}
+            ref={domRef}
+        >
+            {props.children}
 
-    for (var i = 0; i < tags.length; i++){
-        var tag = tags[i];
-        if ($(tag).position().top < pageBottom) {
-            $(tag).addClass("visible");
-        }
-        else{
-            $(tag).removeClass("visible");
-        }
+        </div>
+        );
     }
-});
